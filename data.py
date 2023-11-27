a51ff66c7a09ae1dcac8be0305d60da66947d069
AkshayaR11 <akshayar2005@gmail.com>
2023-11-27 21:38:40 +0530

    Update data.py

--- data.py
+++ data.py
@@ -2,3 +2,58 @@ travel_q={1:'What is the capital of Australia?',2:' Which country is known as th
 ,6:'In which city would you find the famous Shibuya Crossing?',7:'The Ajanta Caves, known for their Buddhist cave paintings, are in which Indian state?',8:'The Louvre,an art and civilization museum,is located in which Middle Eastern country?',9:'Yellowstone National Park, known for its geothermal features, is primarily located in which U.S. state?',10:'How many pyramids are there at the Giza Plateau in Egypt?'}
 travel_a={1:('Canberra','Melbourne','Perth','Canberra','Sydney'),2:('Japan','Nepal','Japan','Indonesia','Ecuador'),3:('Antarctic','Arctic','Sahara','Antarctic','Thar'),4:('Queensland, Australia','Queensland, Australia','Maldives','Mauritius','Thailand'),5:('Dubai International Airport','John F. Kennedy International Airport,New York','Dubai International Airport','Heathrow Airport,London',
 'Changi Airport,Singapore'),6:('Japan','China','Japan','South Korea','Indonesia'),7:('Maharashtra','Odisha','Maharashtra','Kerala','Karnataka'),8:('UAE','Qatar','Oman','UAE','Egypt'),9:('Wyoming','California','Wyoming','Texas','Indiana'),10:('Three','Three','Five','Seven','Two')}
+
+
+#MOVIES
+movie_q={1:'What is the name of the fictional African country in the movie "Black Panther"?',
+         2:"What is the name of the artificial intelligence system that assists the Avengers and is housed in Tony Stark's Iron Man suit?",
+         3:"Which Disney Princess is known for her long, magical hair?",
+         4:"What is the real name of the character Iron Man?",
+         5:"What is the name of the paranormal investigators featured in 'The Conjuring' series?",
+         6:"'Bade bade deshon mein aisi chhoti chhoti baatein hoti rehti hai, Senorita.', is a famous dialogue of which of the following movies?",
+         7:"What was the name of Yash's character in the movie “KGF:chapter 1”",
+         8:"Which movie is considered as the longest-running film in the history of Indian cinema which is being screened in a theatre as of 2023, marking 28years of the movie?",
+         9:"A recent movie to surpass a $1billion box office:",
+         10:"Which was the last movie in the 8-film collection of Harry Potter ?"}
+
+movie_a={1:('Wakanda','kenya','Ghana','Wakanda','naruba'),
+         2:('JARVIS','SYMPHONY','JARVIS','ARIEL','FRIDAY'),
+         3:('Rapunzel','Cinderella','Rapunzel','Snow White','Elsa'),
+         4:('Tony Stark','Tony Stark','Peter Parker','Steve Rogers','Brue Banner'),
+         5:('Ed and Lorraine Warren','Joseph and Anna Warren','Peter and Emma Watson','Annabelle and Isabelle','Ed and Lorraine Warren'),
+         6:('Dilwale Dulhaniya Le Jayenge','Dilwale','Kabhi kushi kabhi Gham','Dilwale Dulhaniya Le Jayenge','Kuch Kuch Hota Hai'),
+         7:('Raja Krishnappa Bairya','Raja Krishnappa Bairya','Raghava Chennappa','Rajkumara Hegde','Santosh Bairya'),
+         8:('Dilwale Wale Dulhaniya Le Jayenge','Sholay','Dilwale Wale Dulhaniya Le Jayenge','Karma','Hum'),
+         9:('Barbie','Oppenheimer','Barbie','Meg 2:The trench','The Exorcist:Believer'),
+         10:('Harry Potter and the Deathly Hallows','Harry Potter and the Half-Blood Prince','Harry Potter and the Goblet of Fire',"Harry Potter and the Sorcerer's Stone",'Harry Potter and the Deathly Hallows')}
+
+
+#BOOKS
+
+books_q={1:" J.R.R. Tolkien wrote 'The Hobbit.' What is the name of the hobbit protagonist?",
+         2:"What is the name of the magical creature that guides Harry Potter to Platform 9¾?",
+         3:"What is the name of the camp where Percy and other demigods are trained?",
+         4:"In which book does Ruskin Bond introduce the character Rusty, a young boy who becomes a recurring figure in many of his stories?",
+         5:"Name Greg Heffley's older brother in the Series 'The Diary of a Wimpy Kid'?",
+         6:"Which Character In the series 'Charlie and The Chocolate Factory' eats a piece of gum and turns into a blueberry?",
+         7:"What is Nancy's father's profession in the series 'Nancy Drew'?",
+         8:"Who is the author of the series 'The Hardy Boys'?",
+         9:"Alongside Hercule Poirot, Agatha Christie created another famous detective character. What is the name of this other detective?",
+         10:"Which March sister gets married first, in the book 'Little Women'?"}
+
+books_a={}
+
+#PYTHON
+
+python_q={1:" How do you comment out multiple lines in Python?",
+          2:"What is the result of the expression 3 * 4 + 5?",
+          3:"What does the len() function do in Python?",
+          4:"How do you check if a key is present in a dictionary?",
+          5:"What is the difference between a tuple and a list in Python?",
+          6:"What is the purpose of the zip() function in Python?",
+          7:"How do you open a file in binary mode in Python?",
+          8:"What is the output of 3 ** 4 in Python?",
+          9:"What is the purpose of the lambda keyword in Python?",
+          10:"How do you concatenate two lists in Python?"}
+
+python_a={}
