fa5b30ed5c2f99cec0d5cd685f70d31a1943cb1c
Keeru-2005 <keeru0000@gmail.com>
2023-12-01 12:09:21 +0530

    pic

--- memeQuiz.py
+++ memeQuiz.py
@@ -3,28 +3,30 @@ print('Python Project')
 from tkinter import *
 import random
 from data import *
-
+from PIL import Image, ImageTk
 root=Tk()
 root.state('zoomed')
 root.configure(background= "#F49393")
 n=1
 score=0
 index=1
-def sel(chb,ans):
+def sel(chb,ans,rootm):
     global index,score
     if index in ans:
         if chb['text']==ans[index][0]:
             score+=10
+            crtmeme=Image.open('./images/image_01.png')
+            crtmeme=crtmeme.resize((150,150))
+            p=ImageTk.PhotoImage(crtmeme)
+            crtlabel=Label(rootm,image=p,text='Correct answer')
+            crtlabel.image=p
+            crtlabel.grid(row=25,column=2)
     index+=1
     
 def T():
     rootm=Toplevel()
-    rootm.geometry('800x500')
-    rootm.withdraw()
-    rootm.deiconify()
+    rootm.state('zoomed')
     def qa(n):
-        rootm.withdraw()
-        rootm.deiconify()
         if n in travel_q:
             label=Label(rootm,text='Your question:',padx=100)
             label.grid(row=4,column=2)
@@ -32,13 +34,13 @@ def T():
             labelQ.grid(row=6,column=2)
             ch=IntVar()
             
-            chb1=Radiobutton(rootm,variable=ch,value=1,text=travel_a[n][1],command=lambda: sel(chb1,travel_a),padx=105)
+            chb1=Radiobutton(rootm,variable=ch,value=1,text=travel_a[n][1],command=lambda: sel(chb1,travel_a,rootm),padx=105)
             chb1.grid(row=10,column=2)
-            chb2=Radiobutton(rootm,variable=ch,value=2,text=travel_a[n][2],command=lambda: sel(chb2,travel_a),padx=105)
+            chb2=Radiobutton(rootm,variable=ch,value=2,text=travel_a[n][2],command=lambda: sel(chb2,travel_a,rootm),padx=105)
             chb2.grid(row=11,column=2)
-            chb3=Radiobutton(rootm,variable=ch,value=3,text=travel_a[n][3],command=lambda: sel(chb3,travel_a),padx=105)
+            chb3=Radiobutton(rootm,variable=ch,value=3,text=travel_a[n][3],command=lambda: sel(chb3,travel_a,rootm),padx=105)
             chb3.grid(row=12,column=2)
-            chb4=Radiobutton(rootm,variable=ch,value=4,text=travel_a[n][4],command=lambda: sel(chb4,travel_a),padx=105)
+            chb4=Radiobutton(rootm,variable=ch,value=4,text=travel_a[n][4],command=lambda: sel(chb4,travel_a,rootm),padx=105)
             chb4.grid(row=13,column=2)
             
             nextB=Button(rootm,text="Next",command=lambda:qa(n+1))
